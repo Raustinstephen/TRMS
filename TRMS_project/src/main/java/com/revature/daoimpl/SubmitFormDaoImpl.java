830548528b6f210fe0d3ec307df7b3490a448e3c
RAustinStephen <r.austinstephen@gmail.com>
2019-03-12 12:59:35 -0400

    submitdao

--- TRMS_project/src/main/java/com/revature/daoimpl/SubmitFormDaoImpl.java
+++ TRMS_project/src/main/java/com/revature/daoimpl/SubmitFormDaoImpl.java
@@ -12,7 +12,7 @@ public class SubmitFormDaoImpl {
 	public static ConnFactory cf = ConnFactory.getInstance();
 	public void submitForm(ReimburstReq req, int EMP_ID) {
 		Connection conn = cf.getConnection();
-		String sql = "{ call INSERT_REIMB_REQ(?,?,?,?,?,?,?,?,?)";
+		String sql = "{ call INSERT_REIMB_REQ(?,?,?,?,?,?,?,?,?,?)";
 		CallableStatement call;
 		try {
 			call = conn.prepareCall(sql);
@@ -20,6 +20,8 @@ public class SubmitFormDaoImpl {
 		double cost = parse.parsingDouble(req.getEventCost());
 		double hours = parse.parsingDouble(req.getHoursMissed());
 		int eventType = parse.parsingInt(req.getEventType());
+		int gradeForm = parse.parsingInt(req.getGradeFormat());
+		System.out.println(eventType + " === " + gradeForm);
 		call.setInt(1, EMP_ID);
 		call.setString(2, req.getJustification());
 		call.setDouble(3, hours);
@@ -28,7 +30,7 @@ public class SubmitFormDaoImpl {
 		call.setString(6, req.getAddress()+req.getCity() + ", " + req.getState());
 		call.setString(7, "2019/03/09 9:00:00");
 		call.setDouble(8, cost);
-		call.setInt(9, 4);
+		call.setInt(9, gradeForm);
 		call.setInt(10, eventType);
 		call.execute();
 		} catch (SQLException e) {
