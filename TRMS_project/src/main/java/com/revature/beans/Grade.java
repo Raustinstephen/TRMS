e506e690bdba44447e5b37f42484a612fa66a771
RAustinStephen <r.austinstephen@gmail.com>
2019-03-13 20:09:36 -0400

    grade servlet

---
+++ TRMS_project/src/main/java/com/revature/beans/Grade.java
@@ -0,0 +1,33 @@
+package com.revature.beans;
+
+public class Grade {
+	private int reimbID;
+	private String grade;
+	public Grade(int reimbID, String grade) {
+		super();
+		this.reimbID = reimbID;
+		this.grade = grade;
+	}
+	public Grade() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getReimbID() {
+		return reimbID;
+	}
+	public void setReimbID(int reimbID) {
+		this.reimbID = reimbID;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	@Override
+	public String toString() {
+		return "Grade [reimbID=" + reimbID + ", grade=" + grade + "]";
+	}
+	
+	
+}
--- TRMS_project/src/main/java/com/revature/servlet/GradeServlet.java
+++ TRMS_project/src/main/java/com/revature/servlet/GradeServlet.java
@@ -1,10 +1,16 @@
 package com.revature.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.beans.Grade;
+import com.revature.daoimpl.UpdateDaoImpl;
 
 /**
  * Servlet implementation class GradeServlet
@@ -32,8 +38,11 @@ public class GradeServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		UpdateDaoImpl udi = new UpdateDaoImpl();
+
+		ObjectMapper om = new ObjectMapper();
+		Grade g = om.readValue(request.getInputStream(), Grade.class);
+		 udi.updateGrade(g.getReimbID(), g.getGrade());
 	}
 
 }
